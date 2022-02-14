0e623fad52b22cf5c1a4217f8bb3f4fc8a91a28f
HVetal <HV2011@inbox.ru>
2022-02-14 22:30:54 +0300

    lesson 3 in work

---
+++ myapp/src/components/MessageList/index.js
@@ -0,0 +1,7 @@
+import { Message } from "../Message"
+
+export const MessageList = ({ messages }) => 
+    messages.map((message) => (
+    <Message key={message.id} text={message.text} author={message.author} />
+    ));
+    
