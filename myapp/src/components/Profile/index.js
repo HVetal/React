38b1db21010e7df777fe05183a5d362a8b105d61
hv2011@inbox.ru <hv2011@inbox.ru>
2022-02-25 16:50:25 +0300

    lesson 5 in work

--- myapp/src/components/Profile/index.js
+++ myapp/src/components/Profile/index.js
@@ -1,1 +1,27 @@
-export const ProfilePage = () => <h2>Profile page body</h2>
+// import { store } from "../../store";
+
+import { useDispatch, useSelector } from "react-redux";
+
+export const Profile = () => {
+    
+    const dispatch = useDispatch();
+    const data = useSelector((state) => state);
+    console.log(data);
+
+    const handleChangeShowName = () => {
+        dispatch(changeShowName)
+    }
+
+    const handleClick = () => {
+        setMessaeColor((prevColor) => ())
+    }
+    // const storeState = store.getState();
+    // console.log('store', storeState);
+    return (
+        <>
+        <h2>Profile page body</h2>;
+        <button onClick={handleClick}></button>
+        </>
+    );
+};
+
---
+++ myapp/src/store/profile/actions.js
@@ -0,0 +1,5 @@
+export const CHANGE_SHOW_NAME = 'PROFILE::CHANGE_SHOW_NAME';
+
+export const cangeShowName = {
+    type: CHANGE_SHOW_NAME,
+};
