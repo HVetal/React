d0296c6afa3e66868bf066e41d812ec875fb1de9
HVetal <HV2011@inbox.ru>
2022-03-10 22:19:51 +0300

    lesson 8 in work

--- myapp/src/components/utils/constants.js
+++ myapp/src/components/utils/constants.js
@@ -4,3 +4,10 @@ export const AUTHORS = {
 };
 
 export const apiUrl = "https://api.spaceflightnewsapi.net/v3/articles";
+
+export const FETCH_STATUSES = {
+    IDLE: 0,
+    REQUEST: 1,
+    SUCCESS: 2,
+    FAILURE: 3,
+}
